8ad3bd75204ffd8539d3867852708f55a2960d8b
Marko Bondaruk <markobondaruk@gmail.com>
2023-08-17 17:35:09 -0600

    index page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+import markdown2
 
 from . import util
 
@@ -8,3 +10,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request, title):
+    entry_content = util.get_entry(title)
+    if entry_content is None:
+        raise Http404("Entry not found")
+    
+    # Convert Markdown to HTML
+    entry_html = markdown2.markdown(entry_content)
+
+    return render(request, "encyclopedia/page.html", {
+        "entry": entry_html, 
+        "title": title.upper()
+    })
